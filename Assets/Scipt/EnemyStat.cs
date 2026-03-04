10ff25f7ffc1f81597122dcb0763943aa646ee59
keplane <123tyreza@gmail.com>
2026-03-04 12:40:45 +0100

    ennemy path direction

--- Assets/Scipt/EnemyStat.cs
+++ Assets/Scipt/EnemyStat.cs
@@ -1,23 +1,46 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStat : MonoBehaviour
 {
     public float Health;
+    public NavMeshAgent Agent;
+    public Transform Player;
+    public float DetectionRange;
+    public float ShootingDistance;
+   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(transform.position, Player.position);
+
+        if (distance <= DetectionRange)
+        {
+
+            if (distance > ShootingDistance )
+            {
+                Agent.SetDestination(Player.position);
+            }
+            else
+            {
+                Agent.ResetPath();
+                transform.LookAt(Player);
+            }
+        }
+
+          //  Agent.SetDestination(Player.position);
 
         if (Health < 0)
         {
            Death();
-
         }
     }
 
@@ -25,5 +48,13 @@ public class EnemyStat : MonoBehaviour
     {
         Destroy(this.gameObject);
         
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, DetectionRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, ShootingDistance);
     }
 }
